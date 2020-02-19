6291a723574ed8b2ce6aa70550c3e9edb386d567
André Karge <andre.karge@uni-weimar.de>
2020-02-19 13:02:51 +0100

    add java socket client

--- java_socket_client/src/main/java/java_socket_client/SocketConnection.java
+++ java_socket_client/src/main/java/java_socket_client/SocketConnection.java
@@ -10,28 +10,48 @@ import java.net.UnknownHostException;
 
 public class SocketConnection {
     private Socket socket;
-    private PrintWriter out;
+    private String ip;
+    private int port;
     private BufferedReader in;
+    private PrintWriter out;
 
-    public void startConnection(String ip, int port) throws UnknownHostException, IOException {
-        socket = new Socket(ip, port);
-        out = new PrintWriter(socket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public SocketConnection(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
     }
 
-    public String sendMessage(String msg) throws IOException {
+    public void connect() throws IOException {
+        this.socket = new Socket(this.ip, this.port);
+        this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        this.out = new PrintWriter(this.socket.getOutputStream(), true);
+    }
+    public void disconnect() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.socket.close();
+    }
+
+//    public void startConnection(String ip, int port) throws UnknownHostException, IOException {
+//        socket = new Socket(ip, port);
+//        out = new PrintWriter(socket.getOutputStream(), true);
+//        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//    }
+
+    public void sendMessage(String msg) throws IOException {
+//        PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
         out.println(msg);
+        System.out.println("message was send to server");
+    }
+
+    public String receiveMessage() throws IOException  {
+//        BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        System.out.println("getting response");
+        boolean asd = in.ready();
         String line = "";
         String response = "";
         while(!(line = in.readLine()).equals("$EOS$")) {
             response += line + "\n";
         }
         return response;
-    }
-
-    public void stopConnection() throws IOException {
-        in.close();
-        out.close();
-        socket.close();
     }
 }
